b282c48333764f5187b659cb1cd36cc50a921e2c
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-01-06 00:55:23 +0100

    Make the script actually work

--- deployment/bare-metal/manual/install.bash
+++ deployment/bare-metal/manual/install.bash
@@ -12,29 +12,27 @@ GNUHEALTH_VERSION=3.6.2
 GNUHEALTH_LOG_CONF=$( cat ${HOME}/health-demo/deployment/bare-metal/manual/files/gnuhealth_log.conf )
 TRYTOND_CONF=$( cat ${HOME}/health-demo/deployment/bare-metal/manual/files/trytond.conf )
 
-# Root access is mandatory.
-sudo -i
 # Install GNU Health dependencies.
 apt-get update
 apt-get install postgresql patch python3-pip unoconv -y
 # GNU Health installation process starts here.
 adduser --disabled-password --gecos "" gnuhealth
-su - gnuhealth
-cd ${HOME}
-wget https://ftp.gnu.org/gnu/health/gnuhealth-${GNUHEALTH_VERSION}.tar.gz
+su - postgres -c "createuser --createdb --no-createrole --no-superuser gnuhealth"
+su - gnuhealth -c "wget https://ftp.gnu.org/gnu/health/gnuhealth-${GNUHEALTH_VERSION}.tar.gz"
 # Verifying the package signature *could* be skipped.
-gpg --keyserver hkps://keyserver.ubuntu.com --recv-key 0xC015E1AE00989199
-gpg --with-fingerprint --list-keys 0xC015E1AE00989199
-wget https://ftp.gnu.org/gnu/health/gnuhealth-${GNUHEALTH_VERSION}.tar.gz.sig
-gpg --verify gnuhealth-${GNUHEALTH_VERSION}.tar.gz.sig gnuhealth-${GNUHEALTH_VERSION}.tar.gz
-tar xzf gnuhealth-${GNUHEALTH_VERSION}.tar.gz
-cd gnuhealth-${GNUHEALTH_VERSION}
-wget -qO- https://ftp.gnu.org/gnu/health/gnuhealth-setup-latest.tar.gz | tar -xzvf -
-bash ./gnuhealth-setup install
-source ${HOME}/.gnuhealthrc
+su - gnuhealth -c "gpg --keyserver hkps://keyserver.ubuntu.com --recv-key 0xC015E1AE00989199"
+su - gnuhealth -c "gpg --with-fingerprint --list-keys 0xC015E1AE00989199"
+su - gnuhealth -c "wget https://ftp.gnu.org/gnu/health/gnuhealth-${GNUHEALTH_VERSION}.tar.gz.sig"
+su - gnuhealth -c "gpg --verify gnuhealth-${GNUHEALTH_VERSION}.tar.gz.sig gnuhealth-${GNUHEALTH_VERSION}.tar.gz"
+su - gnuhealth -c "tar xzf gnuhealth-${GNUHEALTH_VERSION}.tar.gz"
+su - gnuhealth \
+    -c "cd gnuhealth-${GNUHEALTH_VERSION} && \
+        wget -qO- https://ftp.gnu.org/gnu/health/gnuhealth-setup-latest.tar.gz | tar -xzvf - && \
+        bash ./gnuhealth-setup install"
 # Write config files.
-echo "${GNUHEALTH_LOG_CONF}" > ${GNUHEALTH_DIR}/tryton/server/config/gnuhealth_log.conf
-echo "${TRYTOND_CONF}" > ${GNUHEALTH_DIR}/tryton/server/config/trytond.conf
+su - gnuhealth \
+    -c "source \${HOME}/.gnuhealthrc && \
+        echo \"${GNUHEALTH_LOG_CONF}\" > \${GNUHEALTH_DIR}/tryton/server/config/gnuhealth_log.conf && \
+        echo \"${TRYTOND_CONF}\" > \${GNUHEALTH_DIR}/tryton/server/config/trytond.conf"
 # For the sake of the demo - populate database with demo content.
-cd ${GNUHEALTH_DIR}/scripts/demo
-./install_demo_database.sh 36
+su - gnuhealth -c "cd gnuhealth-${GNUHEALTH_VERSION}/scripts/demo && ./install_demo_database.sh 36"
